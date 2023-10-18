15d6115fe0b20a845f45200884ac61feca219c32
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2023-10-18 16:05:01 +0100

    added go tests

--- deck.go
+++ deck.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -34,4 +37,30 @@ func deal(d deck, handSize int ) (deck, deck){
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 	
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0644)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",") 
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
